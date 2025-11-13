c9a29f429ec0f90a87a1efae99fb3c2f49f1c3c8
rinaeshin <rinaeshin@gmail.com>
2025-11-13 09:14:52 +0900

    DOCS : Attend 및 Schedule 구현을 위한 컨트롤러 주석 추가

---
+++ src/main/java/com/example/attempt/controller/ScheduleController.java
@@ -0,0 +1,9 @@
+package com.example.attempt.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScheduleController {
+    // https://junesker.tistory.com/125
+    // 플 캘린더 기반 일정 관리 - 노인 일자리 참여자 본인 스케줄 확인 가능 및 변경 사항이 DB에 첨부되어야 함.
+}
