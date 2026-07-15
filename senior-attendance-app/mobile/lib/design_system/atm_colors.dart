e42f88654ae7be5ffe94419266e9a0aa55709d84
rinaeshin <rinaeshin@gmail.com>
2026-07-15 10:49:02 +0900

    feat(design-system): add AtmColors and AtmPrimaryButton

---
+++ senior-attendance-app/mobile/lib/design_system/atm_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AtmColors {
+  static const primary = Color(0xFF2E9E4F);
+  static const secondary = Color(0xFFF5821F);
+  static const background = Color(0xFFF7F7F7);
+}
